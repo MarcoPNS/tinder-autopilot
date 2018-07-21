4211ea83910c410be1b401d09b9698bd9ec3a7dd
Marco Sadowski <marco@patchnotestudio.com>
2018-07-21 22:24:05 +0200

    I cant remember what I did but its getting awesome... I guess

--- TinderAutopilot.AdminPanel/MainViewer.Designer.vb
+++ TinderAutopilot.AdminPanel/MainViewer.Designer.vb
@@ -22,15 +22,29 @@ Partial Class MainViewer
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainViewer))
         Me.MetroTabControl1 = New MetroFramework.Controls.MetroTabControl()
         Me.MetroTabPage1 = New MetroFramework.Controls.MetroTabPage()
+        Me.SaveAuth = New MetroFramework.Controls.MetroButton()
+        Me.TinderAuthBox = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroLabel3 = New MetroFramework.Controls.MetroLabel()
+        Me.FacebookAuthBox = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroLabel2 = New MetroFramework.Controls.MetroLabel()
         Me.MetroListView1 = New MetroFramework.Controls.MetroListView()
         Me.MetroLabel1 = New MetroFramework.Controls.MetroLabel()
         Me.MetroTabPage2 = New MetroFramework.Controls.MetroTabPage()
         Me.MetroTabPage3 = New MetroFramework.Controls.MetroTabPage()
-        Me.MetroToggle1 = New MetroFramework.Controls.MetroToggle()
+        Me.LogBox = New System.Windows.Forms.TextBox()
+        Me.FireSwitch = New MetroFramework.Controls.MetroToggle()
+        Me.TinderWorker = New System.ComponentModel.BackgroundWorker()
+        Me.OnFire = New System.Windows.Forms.PictureBox()
+        Me.FacebookAccIDBox = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroLabel4 = New MetroFramework.Controls.MetroLabel()
+        Me.StatusLabel = New System.Windows.Forms.Label()
         Me.MetroTabControl1.SuspendLayout()
         Me.MetroTabPage1.SuspendLayout()
+        Me.MetroTabPage3.SuspendLayout()
+        CType(Me.OnFire, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MetroTabControl1
@@ -38,15 +52,23 @@ Partial Class MainViewer
         Me.MetroTabControl1.Controls.Add(Me.MetroTabPage1)
         Me.MetroTabControl1.Controls.Add(Me.MetroTabPage2)
         Me.MetroTabControl1.Controls.Add(Me.MetroTabPage3)
-        Me.MetroTabControl1.Location = New System.Drawing.Point(23, 63)
+        Me.MetroTabControl1.Location = New System.Drawing.Point(22, 63)
         Me.MetroTabControl1.Name = "MetroTabControl1"
         Me.MetroTabControl1.SelectedIndex = 0
-        Me.MetroTabControl1.Size = New System.Drawing.Size(754, 374)
+        Me.MetroTabControl1.Size = New System.Drawing.Size(754, 364)
+        Me.MetroTabControl1.Style = MetroFramework.MetroColorStyle.Orange
         Me.MetroTabControl1.TabIndex = 0
         Me.MetroTabControl1.UseSelectable = True
         '
         'MetroTabPage1
         '
+        Me.MetroTabPage1.Controls.Add(Me.FacebookAccIDBox)
+        Me.MetroTabPage1.Controls.Add(Me.MetroLabel4)
+        Me.MetroTabPage1.Controls.Add(Me.SaveAuth)
+        Me.MetroTabPage1.Controls.Add(Me.TinderAuthBox)
+        Me.MetroTabPage1.Controls.Add(Me.MetroLabel3)
+        Me.MetroTabPage1.Controls.Add(Me.FacebookAuthBox)
+        Me.MetroTabPage1.Controls.Add(Me.MetroLabel2)
         Me.MetroTabPage1.Controls.Add(Me.MetroListView1)
         Me.MetroTabPage1.Controls.Add(Me.MetroLabel1)
         Me.MetroTabPage1.HorizontalScrollbarBarColor = True
@@ -54,21 +76,108 @@ Partial Class MainViewer
         Me.MetroTabPage1.HorizontalScrollbarSize = 10
         Me.MetroTabPage1.Location = New System.Drawing.Point(4, 38)
         Me.MetroTabPage1.Name = "MetroTabPage1"
-        Me.MetroTabPage1.Size = New System.Drawing.Size(746, 332)
+        Me.MetroTabPage1.Size = New System.Drawing.Size(746, 322)
         Me.MetroTabPage1.TabIndex = 0
         Me.MetroTabPage1.Text = "General Settings"
         Me.MetroTabPage1.VerticalScrollbarBarColor = True
         Me.MetroTabPage1.VerticalScrollbarHighlightOnWheel = False
         Me.MetroTabPage1.VerticalScrollbarSize = 10
         '
+        'SaveAuth
+        '
+        Me.SaveAuth.Location = New System.Drawing.Point(422, 27)
+        Me.SaveAuth.Name = "SaveAuth"
+        Me.SaveAuth.Size = New System.Drawing.Size(56, 23)
+        Me.SaveAuth.TabIndex = 8
+        Me.SaveAuth.Text = "Save"
+        Me.SaveAuth.UseSelectable = True
+        '
+        'TinderAuthBox
+        '
+        '
+        '
+        '
+        Me.TinderAuthBox.CustomButton.Image = Nothing
+        Me.TinderAuthBox.CustomButton.Location = New System.Drawing.Point(103, 1)
+        Me.TinderAuthBox.CustomButton.Name = ""
+        Me.TinderAuthBox.CustomButton.Size = New System.Drawing.Size(21, 21)
+        Me.TinderAuthBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue
+        Me.TinderAuthBox.CustomButton.TabIndex = 1
+        Me.TinderAuthBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light
+        Me.TinderAuthBox.CustomButton.UseSelectable = True
+        Me.TinderAuthBox.CustomButton.Visible = False
+        Me.TinderAuthBox.Lines = New String(-1) {}
+        Me.TinderAuthBox.Location = New System.Drawing.Point(291, 27)
+        Me.TinderAuthBox.MaxLength = 32767
+        Me.TinderAuthBox.Name = "TinderAuthBox"
+        Me.TinderAuthBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.TinderAuthBox.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.TinderAuthBox.SelectedText = ""
+        Me.TinderAuthBox.SelectionLength = 0
+        Me.TinderAuthBox.SelectionStart = 0
+        Me.TinderAuthBox.ShortcutsEnabled = True
+        Me.TinderAuthBox.Size = New System.Drawing.Size(125, 23)
+        Me.TinderAuthBox.TabIndex = 7
+        Me.TinderAuthBox.UseSelectable = True
+        Me.TinderAuthBox.WaterMarkColor = System.Drawing.Color.FromArgb(CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.TinderAuthBox.WaterMarkFont = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel)
+        '
+        'MetroLabel3
+        '
+        Me.MetroLabel3.AutoSize = True
+        Me.MetroLabel3.Location = New System.Drawing.Point(291, 5)
+        Me.MetroLabel3.Name = "MetroLabel3"
+        Me.MetroLabel3.Size = New System.Drawing.Size(125, 19)
+        Me.MetroLabel3.TabIndex = 6
+        Me.MetroLabel3.Text = "Tinder Access Token"
+        '
+        'FacebookAuthBox
+        '
+        '
+        '
+        '
+        Me.FacebookAuthBox.CustomButton.Image = Nothing
+        Me.FacebookAuthBox.CustomButton.Location = New System.Drawing.Point(122, 1)
+        Me.FacebookAuthBox.CustomButton.Name = ""
+        Me.FacebookAuthBox.CustomButton.Size = New System.Drawing.Size(21, 21)
+        Me.FacebookAuthBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue
+        Me.FacebookAuthBox.CustomButton.TabIndex = 1
+        Me.FacebookAuthBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light
+        Me.FacebookAuthBox.CustomButton.UseSelectable = True
+        Me.FacebookAuthBox.CustomButton.Visible = False
+        Me.FacebookAuthBox.Lines = New String(-1) {}
+        Me.FacebookAuthBox.Location = New System.Drawing.Point(141, 27)
+        Me.FacebookAuthBox.MaxLength = 32767
+        Me.FacebookAuthBox.Name = "FacebookAuthBox"
+        Me.FacebookAuthBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.FacebookAuthBox.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.FacebookAuthBox.SelectedText = ""
+        Me.FacebookAuthBox.SelectionLength = 0
+        Me.FacebookAuthBox.SelectionStart = 0
+        Me.FacebookAuthBox.ShortcutsEnabled = True
+        Me.FacebookAuthBox.Size = New System.Drawing.Size(144, 23)
+        Me.FacebookAuthBox.TabIndex = 5
+        Me.FacebookAuthBox.UseSelectable = True
+        Me.FacebookAuthBox.WaterMarkColor = System.Drawing.Color.FromArgb(CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FacebookAuthBox.WaterMarkFont = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel)
+        '
+        'MetroLabel2
+        '
+        Me.MetroLabel2.AutoSize = True
+        Me.MetroLabel2.Location = New System.Drawing.Point(141, 5)
+        Me.MetroLabel2.Name = "MetroLabel2"
+        Me.MetroLabel2.Size = New System.Drawing.Size(144, 19)
+        Me.MetroLabel2.TabIndex = 4
+        Me.MetroLabel2.Text = "Facebook OAuth Token"
+        '
         'MetroListView1
         '
         Me.MetroListView1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
         Me.MetroListView1.FullRowSelect = True
-        Me.MetroListView1.Location = New System.Drawing.Point(3, 118)
+        Me.MetroListView1.Location = New System.Drawing.Point(3, 75)
         Me.MetroListView1.Name = "MetroListView1"
         Me.MetroListView1.OwnerDraw = True
-        Me.MetroListView1.Size = New System.Drawing.Size(176, 208)
+        Me.MetroListView1.Size = New System.Drawing.Size(102, 208)
         Me.MetroListView1.TabIndex = 3
         Me.MetroListView1.UseCompatibleStateImageBehavior = False
         Me.MetroListView1.UseSelectable = True
@@ -76,11 +185,11 @@ Partial Class MainViewer
         'MetroLabel1
         '
         Me.MetroLabel1.AutoSize = True
-        Me.MetroLabel1.Location = New System.Drawing.Point(3, 96)
+        Me.MetroLabel1.Location = New System.Drawing.Point(3, 53)
         Me.MetroLabel1.Name = "MetroLabel1"
-        Me.MetroLabel1.Size = New System.Drawing.Size(102, 19)
+        Me.MetroLabel1.Size = New System.Drawing.Size(101, 19)
         Me.MetroLabel1.TabIndex = 2
-        Me.MetroLabel1.Text = "Additional Filter"
+        Me.MetroLabel1.Text = "Instant Like Tags"
         '
         'MetroTabPage2
         '
@@ -98,6 +207,7 @@ Partial Class MainViewer
         '
         'MetroTabPage3
         '
+        Me.MetroTabPage3.Controls.Add(Me.LogBox)
         Me.MetroTabPage3.HorizontalScrollbarBarColor = True
         Me.MetroTabPage3.HorizontalScrollbarHighlightOnWheel = False
         Me.MetroTabPage3.HorizontalScrollbarSize = 10
@@ -110,15 +220,88 @@ Partial Class MainViewer
         Me.MetroTabPage3.VerticalScrollbarHighlightOnWheel = False
         Me.MetroTabPage3.VerticalScrollbarSize = 10
         '
-        'MetroToggle1
+        'LogBox
         '
-        Me.MetroToggle1.AutoSize = True
-        Me.MetroToggle1.Location = New System.Drawing.Point(693, 40)
-        Me.MetroToggle1.Name = "MetroToggle1"
-        Me.MetroToggle1.Size = New System.Drawing.Size(80, 17)
-        Me.MetroToggle1.TabIndex = 1
-        Me.MetroToggle1.Text = "Aus"
-        Me.MetroToggle1.UseSelectable = True
+        Me.LogBox.Location = New System.Drawing.Point(0, 3)
+        Me.LogBox.Multiline = True
+        Me.LogBox.Name = "LogBox"
+        Me.LogBox.Size = New System.Drawing.Size(743, 326)
+        Me.LogBox.TabIndex = 2
+        '
+        'FireSwitch
+        '
+        Me.FireSwitch.AutoSize = True
+        Me.FireSwitch.Location = New System.Drawing.Point(198, 30)
+        Me.FireSwitch.Name = "FireSwitch"
+        Me.FireSwitch.Size = New System.Drawing.Size(80, 17)
+        Me.FireSwitch.Style = MetroFramework.MetroColorStyle.Orange
+        Me.FireSwitch.TabIndex = 1
+        Me.FireSwitch.Text = "Aus"
+        Me.FireSwitch.Theme = MetroFramework.MetroThemeStyle.Light
+        Me.FireSwitch.UseSelectable = True
+        '
+        'TinderWorker
+        '
+        Me.TinderWorker.WorkerReportsProgress = True
+        Me.TinderWorker.WorkerSupportsCancellation = True
+        '
+        'OnFire
+        '
+        Me.OnFire.Image = Global.TinderAutopilot.AdminPanel.My.Resources.Resources.fire
+        Me.OnFire.Location = New System.Drawing.Point(8, 30)
+        Me.OnFire.Name = "OnFire"
+        Me.OnFire.Size = New System.Drawing.Size(13, 15)
+        Me.OnFire.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.OnFire.TabIndex = 2
+        Me.OnFire.TabStop = False
+        '
+        'FacebookAccIDBox
+        '
+        '
+        '
+        '
+        Me.FacebookAccIDBox.CustomButton.Image = Nothing
+        Me.FacebookAccIDBox.CustomButton.Location = New System.Drawing.Point(110, 1)
+        Me.FacebookAccIDBox.CustomButton.Name = ""
+        Me.FacebookAccIDBox.CustomButton.Size = New System.Drawing.Size(21, 21)
+        Me.FacebookAccIDBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue
+        Me.FacebookAccIDBox.CustomButton.TabIndex = 1
+        Me.FacebookAccIDBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light
+        Me.FacebookAccIDBox.CustomButton.UseSelectable = True
+        Me.FacebookAccIDBox.CustomButton.Visible = False
+        Me.FacebookAccIDBox.Lines = New String(-1) {}
+        Me.FacebookAccIDBox.Location = New System.Drawing.Point(3, 27)
+        Me.FacebookAccIDBox.MaxLength = 32767
+        Me.FacebookAccIDBox.Name = "FacebookAccIDBox"
+        Me.FacebookAccIDBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.FacebookAccIDBox.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.FacebookAccIDBox.SelectedText = ""
+        Me.FacebookAccIDBox.SelectionLength = 0
+        Me.FacebookAccIDBox.SelectionStart = 0
+        Me.FacebookAccIDBox.ShortcutsEnabled = True
+        Me.FacebookAccIDBox.Size = New System.Drawing.Size(132, 23)
+        Me.FacebookAccIDBox.TabIndex = 10
+        Me.FacebookAccIDBox.UseSelectable = True
+        Me.FacebookAccIDBox.WaterMarkColor = System.Drawing.Color.FromArgb(CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FacebookAccIDBox.WaterMarkFont = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel)
+        '
+        'MetroLabel4
+        '
+        Me.MetroLabel4.AutoSize = True
+        Me.MetroLabel4.Location = New System.Drawing.Point(3, 5)
+        Me.MetroLabel4.Name = "MetroLabel4"
+        Me.MetroLabel4.Size = New System.Drawing.Size(132, 19)
+        Me.MetroLabel4.TabIndex = 9
+        Me.MetroLabel4.Text = "Facebook Account ID"
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.AutoSize = True
+        Me.StatusLabel.Location = New System.Drawing.Point(26, 430)
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(124, 13)
+        Me.StatusLabel.TabIndex = 3
+        Me.StatusLabel.Text = "Hi! This is a Status Label"
         '
         'MainViewer
         '
@@ -126,13 +309,23 @@ Partial Class MainViewer
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.MetroToggle1)
+        Me.Controls.Add(Me.StatusLabel)
+        Me.Controls.Add(Me.OnFire)
+        Me.Controls.Add(Me.FireSwitch)
         Me.Controls.Add(Me.MetroTabControl1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
         Me.Name = "MainViewer"
+        Me.Resizable = False
+        Me.ShadowType = MetroFramework.Forms.MetroFormShadowType.DropShadow
+        Me.Style = MetroFramework.MetroColorStyle.Orange
         Me.Text = "Tinder Autopilot"
         Me.MetroTabControl1.ResumeLayout(False)
         Me.MetroTabPage1.ResumeLayout(False)
         Me.MetroTabPage1.PerformLayout()
+        Me.MetroTabPage3.ResumeLayout(False)
+        Me.MetroTabPage3.PerformLayout()
+        CType(Me.OnFire, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -144,5 +337,16 @@ Partial Class MainViewer
     Friend WithEvents MetroTabPage3 As MetroFramework.Controls.MetroTabPage
     Friend WithEvents MetroListView1 As MetroFramework.Controls.MetroListView
     Friend WithEvents MetroLabel1 As MetroFramework.Controls.MetroLabel
-    Friend WithEvents MetroToggle1 As MetroFramework.Controls.MetroToggle
+    Friend WithEvents FireSwitch As MetroFramework.Controls.MetroToggle
+    Friend WithEvents TinderWorker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents OnFire As PictureBox
+    Friend WithEvents FacebookAuthBox As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroLabel2 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents LogBox As TextBox
+    Friend WithEvents TinderAuthBox As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroLabel3 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents SaveAuth As MetroFramework.Controls.MetroButton
+    Friend WithEvents FacebookAccIDBox As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroLabel4 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents StatusLabel As Label
 End Class
--- TinderAutopilot.AdminPanel/MainViewer.vb
+++ TinderAutopilot.AdminPanel/MainViewer.vb
@@ -1,25 +1,63 @@
-﻿Imports SharpTinder
+﻿Imports RestSharp
 Public Class MainViewer
 
     'Das Tinder Telefon
-    Dim Client As New TinderClient
+    Dim Client As New RestClient("https://api.gotinder.com/")
     Dim userId As String = "100001706025627"
-    Dim accessToken As String = "EAAGm0PX4ZCpsBAAZB3nVs08icGbDVxXZCDc5FF8gHn0DXb79c6XXx0qZCTJCD5uwaZBa0ORpZCAV6DicwPsbUIrIU70UP7C67ZA0UfhKf2Ifqt0jnntZBP9ndPZCMZAHE3b5wgjm83LSVpCqZB2eaLc8ywWCXmb9t5xSCgjLovOVavNqohRBgEJEDjxXjhXS9E9rGuBJZBKFhuTSaQZDZD"
+    Dim accessToken As String = "t"
     'Beginn der Action
-    Private Sub MainViewer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim t = Client.Login(userId, accessToken)
-        Task.WaitAll(New Task() {t})
-        Console.WriteLine("Token: " & Client.AuthToken)
-        Console.WriteLine("logged in: " & t.Result)
-        GetTinderMatches()
+    Private Sub ViewerVisible(sender As Object, e As EventArgs) Handles MyBase.Shown
+        Log("Tinder Autopilot Dashboard started")
+        'LoginToTinder()
+        'GetTinderMatches()
+    End Sub
+    Private Sub LoginToTinder()
+
     End Sub
     Private Sub GetTinderMatches()
-        Dim t = Client.GetUpdates(New DateTime(2000, 1, 1))
-        Task.WaitAll(New Task() {t})
-        Dim res = t.Result
 
-        For Each m In res.Matches
-            If m.Person IsNot Nothing Then Console.WriteLine($"{m.Person.Name} ({m.Id}) - {2016 - m.Person.BirthDate.Year}")
-        Next
+    End Sub
+
+    Private Sub FireSwitched(sender As Object, e As EventArgs) Handles FireSwitch.CheckedChanged
+        If FireSwitch.Checked = True Then
+            OnFire.Visible = True
+            TinderWorker.RunWorkerAsync()
+        Else
+            OnFire.Visible = False
+            TinderWorker.CancelAsync()
+        End If
+    End Sub
+    Public Sub TinderAuth()
+
+    End Sub
+    Public Sub GetNewProfile()
+
+    End Sub
+
+    Private Sub TinderWorker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles TinderWorker.DoWork
+
+    End Sub
+
+    Private Sub LoadForm(sender As Object, e As EventArgs) Handles MyBase.Load
+        FacebookAuthBox.Text = My.Settings.FacebookAuth
+        TinderAuthBox.Text = My.Settings.TinderAuth
+        FacebookAccIDBox.Text = My.Settings.FacebookID
+    End Sub
+    Public Sub Log(msg As String)
+        LogBox.AppendText(msg & vbNewLine)
+        StatusLabel.Text = (msg)
+    End Sub
+    Public Sub StartTheWork()
+
+    End Sub
+    Private Sub SavingAuthCodes(sender As Object, e As EventArgs) Handles SaveAuth.Click
+        My.Settings.FacebookAuth = FacebookAuthBox.Text
+        My.Settings.TinderAuth = TinderAuthBox.Text
+        My.Settings.FacebookID = FacebookAccIDBox.Text
+        My.Settings.Save()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles StatusLabel.Click
+
     End Sub
 End Class
