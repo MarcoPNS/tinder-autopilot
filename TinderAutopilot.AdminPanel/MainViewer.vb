f21e62eb480e4a3e8ef0b090ba99b7e0e2cc7963
Marco Sadowski <marco@patchnotestudio.com>
2018-07-21 19:16:44 +0200

    Versuch mit Tinder Sharp

--- TinderAutopilot.AdminPanel/MainViewer.vb
+++ TinderAutopilot.AdminPanel/MainViewer.vb
@@ -1,7 +1,25 @@
 ﻿Imports SharpTinder
 Public Class MainViewer
+
+    'Das Tinder Telefon
+    Dim Client As New TinderClient
+    Dim userId As String = "100001706025627"
+    Dim accessToken As String = "EAAGm0PX4ZCpsBAAZB3nVs08icGbDVxXZCDc5FF8gHn0DXb79c6XXx0qZCTJCD5uwaZBa0ORpZCAV6DicwPsbUIrIU70UP7C67ZA0UfhKf2Ifqt0jnntZBP9ndPZCMZAHE3b5wgjm83LSVpCqZB2eaLc8ywWCXmb9t5xSCgjLovOVavNqohRBgEJEDjxXjhXS9E9rGuBJZBKFhuTSaQZDZD"
+    'Beginn der Action
     Private Sub MainViewer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim Client As New TinderClient
-        Client.Login("100001706025627", "EAAGm0PX4ZCpsBANeedEMNneOb5dyOIMNd3YMpMpKlXNPaTFJgc4RCoORYSNQXnXQtBiMk3JNHKxBCeZAO533qPq7ekQzJFkxLFZCauG9SdQZAhwriEyFNnERbPC40as22UH1x896adCERFkwon94m8ZBuefQ6MMFAihuPYx98r2fxG2py9aoTgikuSPTlBkewHAVXFO4zUILMyaQ5AtH9ZB3i1NN7Q24ZBoTYur1hpf5UcuPQCOn2m5fSLtYc1ZCwzcZD")
+        Dim t = Client.Login(userId, accessToken)
+        Task.WaitAll(New Task() {t})
+        Console.WriteLine("Token: " & Client.AuthToken)
+        Console.WriteLine("logged in: " & t.Result)
+        GetTinderMatches()
+    End Sub
+    Private Sub GetTinderMatches()
+        Dim t = Client.GetUpdates(New DateTime(2000, 1, 1))
+        Task.WaitAll(New Task() {t})
+        Dim res = t.Result
+
+        For Each m In res.Matches
+            If m.Person IsNot Nothing Then Console.WriteLine($"{m.Person.Name} ({m.Id}) - {2016 - m.Person.BirthDate.Year}")
+        Next
     End Sub
 End Class
